c1d33fd4968c11f33ce49fb07853263484f435a0
Cristobal Requena <46720218+crisjose@users.noreply.github.com>
2026-03-08 19:39:32 -0400

    Add files via upload

---
+++ among us/▶ IMG-Biblioteca.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="▶ IMG-Biblioteca" tilewidth="32" tileheight="32" tilecount="3634" columns="46">
+ <image source="C:/Users/reque/Downloads/▶ IMG-Biblioteca.png" width="1474" height="2552"/>
+</tileset>
