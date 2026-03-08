a4f6fca74d809cc9666fad2c9ec557d117d8f6b7
Cristobal Requena <46720218+crisjose@users.noreply.github.com>
2026-03-08 19:41:59 -0400

    Add files via upload

---
+++ among us/plane2t.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="plane2t" tilewidth="32" tileheight="32" tilecount="525" columns="25">
+ <image source="C:/Users/reque/Downloads/plane2t.png" width="820" height="684"/>
+</tileset>
